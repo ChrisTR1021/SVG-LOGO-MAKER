5803e5f12365a00760a5e379a5018641ab00a82a
ChrisTR1021 <christianridgell1021@gmail.com>
2024-01-07 03:58:01 -0500

    fixed syntax errors that would let app run

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const shapeGroup = require("./lib/shapeGroup.js");
-const fileName = 
+const fileName = "./logo.svg";
 
 
 //questions that will prompted
 const questions = [
     {
         type: 'input',
-        message: 'What three letters would you like to use for your logo?'
+        message: 'What three letters would you like to use for your logo?',
         name: 'logoText',
         validate: (answer) => {
             if (answer.length > 3) {
@@ -19,18 +19,18 @@ const questions = [
     },
     {
         type: 'list',
-        message: 'what shape would you like to use for your logo?'
+        message: 'what shape would you like to use for your logo?',
         name: 'shape',
         choices: ['Square', 'Triangle', 'Circle'],
     },
     {
         type: 'input',
-        message: 'what color text would you like for your logo?'
+        message: 'what color text would you like for your logo?',
         name: 'logoColor',
     },
     {
         type: 'input',
-        message: 'what color would you like the shape to be?'
+        message: 'what color would you like the shape to be?',
         name: 'shapeColor',
     },
 ];

47d199b1c7d9b44ed982ae32ea2a53667de044dd
ChrisTR1021 <christianridgell1021@gmail.com>
2024-01-07 02:10:42 -0500

    question order fix

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const inquirer = require("inquirer");
 const fs = require("fs");
+const inquirer = require("inquirer");
 const setShape = 
 const fileName = 
 
 
 //questions that will prompted
 const questions = [
-    {
-        type: 'input',
-        message: 'what color text would you like for your logo?'
-        name: 'logoColor',
-    },
-    {
-        type: 'list',
-        message: 'what shape would you like to use for your logo?'
-        name: 'shape',
-        choices: ['Triangle', 'Circle', 'Square'],
-    },
     {
         type: 'input',
         message: 'What three letters would you like to use for your logo?'
@@ -29,11 +18,27 @@ const questions = [
         }
     },
     {
+        type: 'list',
+        message: 'what shape would you like to use for your logo?'
+        name: 'shape',
+        choices: ['Triangle', 'Circle', 'Square'],
+    },
+    {
+        type: 'input',
+        message: 'what color text would you like for your logo?'
+        name: 'logoColor',
+    },
+    {
         type: 'input',
         message: 'what color would you like the shape to be?'
         name: 'shapeColor',
     },
 ];
+// creates svg and writes it to file
+function logoCreation(response) {
+    const svg = setShape(response);
+    fs.writeFile(fileName, svg, () => console.log("Generated logo.svg"));
+}
 
 function userPrompt() {
     return inquirer.prompt(questions);
